a5afd4d36baa71a9d859ad2130461202745192a1
omar <omarboudaden52@gmail.com>
2023-12-05 18:32:12 +0100

    omar003

---
+++ Controllers/UtilisateurController1.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskBuddy.Data;
+using TaskBuddy.Models;
+
+namespace TaskBuddy.Controllers
+{
+    public class UtilisateurController1 : Controller
+    {
+        private readonly ApplicationDbContext _dbcontext;
+        public UtilisateurController1(ApplicationDbContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+
+
+        [HttpGet]
+        public IActionResult listeuser()
+        {
+            var listeuser = _dbcontext.Utilisateur.ToList();
+
+            return View(listeuser);
+        }
+
+        [HttpGet]
+        public IActionResult crerUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult crerUser(Utilisateur stagire)
+        {
+            _dbcontext.Utilisateur.Add(stagire);
+            _dbcontext.SaveChanges();
+            return RedirectToAction(nameof(listeuser));
+        }
+
+        [HttpGet]
+        public IActionResult afficher(int id)
+        {
+            var user = _dbcontext.Utilisateur.SingleOrDefault(i => i.IdUtilisateur == id);
+            return View(user);
+        }
+
+        [HttpGet]
+        public IActionResult supprimer(int id)
+        {
+            var user = _dbcontext.Utilisateur.Find(id);
+            return View(user);
+        }
+        [HttpPost]
+        public IActionResult supprimer(Utilisateur user)
+        {
+            _dbcontext.Utilisateur.Remove(user);
+            _dbcontext.SaveChanges();
+            return RedirectToAction(nameof(listeuser));
+        }
+
+        [HttpGet]
+        public IActionResult modifier(int id)
+        {
+            var user = _dbcontext.Utilisateur.SingleOrDefault(i => i.IdUtilisateur == id);
+            return View(user);
+        }
+        [HttpPost]
+        public IActionResult modifier(Utilisateur newUser)
+        {
+            var oldUser = _dbcontext.Utilisateur.Find(newUser.IdUtilisateur);
+            if (oldUser != null)
+            {
+                if (oldUser.Type == newUser.Type)
+                {
+                    oldUser.Nom = newUser.Nom;
+                    oldUser.Prenom = newUser.Prenom;
+                    oldUser.Adresse = newUser.Adresse;
+                    oldUser.Tel = newUser.Tel;
+                    oldUser.Photo = newUser.Photo;
+                    oldUser.Ville = newUser.Ville;
+                    oldUser.Score = newUser.Score;
+                    _dbcontext.SaveChanges();
+                }
+            }
+
+            return RedirectToAction(nameof(listeuser));
+        }
+    }
+}
---
+++ Models/Utilisateur.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskBuddy.Models
+{
+    public class Utilisateur
+    {
+        [Key]
+        public int IdUtilisateur { get; set; }
+        [Required]
+        public string? Type { get; set; }
+        [Required]
+        public string? Nom { get; set; }
+        [Required]
+        public string? Prenom { get; set; }
+        [Required]
+
+        public string? Adresse { get; set; }
+        [Required]
+        public int? Tel { get; set; }
+        [Required]
+        public string? Ville { get; set; }
+        [Required]
+        public string? Email { get; set; }
+        [Required]
+        public string? Passwd { get; set; }
+        [Required]
+        public int? Score { get; set; }
+        [Required]
+        public string? Photo { get; set; }
+    }
+}
